4031b0cb4962ac637dd64c414bbeec40af64426a
Aleksandar Bursać <aleksandarbursac150699@gmail.com>
2024-07-01 12:42:43 +0200

    Connecting react to node(server part)

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
-const db = require('./models')
+const db = require('./models');
 //Routers
 const postRouter = require('./routes/Posts');
 app.use("/posts", postRouter);
